d7b4ab964b6ed89cb0136ac46f0f88821ff4fd44
Paavo <pparkkin@gmail.com>
2017-08-15 13:41:18 +0800

    Add readline support.

--- prompt.c
+++ prompt.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#include <editline/readline.h>
+#include <editline/history.h>
 
 int main(int argc, char **argv) {
-  puts("MyLISP v. 0.0.1-SNAPSHOT");
+  puts("MyLISP v. 0.0.2-SNAPSHOT");
   puts("Press Ctrl-c to exit.\n");
 
   while (1) {
-    fputs("ml> ", stdout);
-    fgets(input, 2048, stdin);
-    printf("Echo: %s", input);
+    char *input = readline("ml> ");
+    add_history(input);
+    printf("Echo: %s\n", input);
+    free(input);
   }
 
   return 0;
